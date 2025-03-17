149741e5019bbf12dc311881e7be259af2dc6670
Emily Wu <emily.wu0718@gmail.com>
2025-03-17 12:45:24 -0400

    worked on data sort

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,17 +1,31 @@
 let data = [
-  { rank: 0, name: "dog", score: 0, color: "#66bb6a" },
-  { rank: 0, name: "horse", score: 0, color: "#a1887f" },
-  { rank: 0, name: "dove", score: 0, color: "#42a5f5" },
-  { rank: 0, name: "cat", score: 0, color: "#ffa726" },
-  { rank: 0, name: "spider", score: 0, color: "#ef5350" },
-  { rank: 0, name: "fish", score: 0, color: "#5c6bc0" },
+  {name:"bob", score:10},
+  {name:"jim", score:100},
+  {name:"joe", score:0},
 ];
 
-data
-.sort ((a,b) => {
-  if (b.score === a.score) {
-    return a.name.localeCompare(b.name); //sort by name if scores are equal
-  }
-   return b.score - a.score; //sort by score in descending order
+//display leaderboard
+function displayLeaderboard() {
+  const leaderboard = document.getElementById('leaderboard');
+  leaderboard.innerHTML = ''; //clears the previous content in table
+
+
+  data
+    .sort ((a,b) => {
+      if (b.score === a.score) {
+        return a.name.localeCompare(b.name); //sort by name if scores are equal
+      } else
+      return b.score - a.score; //sort by score in descending order
+    });
+
+  data.forEach((data, index) => {
+    let row = `<tr>
+        <td>${index + 1}</td>
+        <td>${data.name}</td>
+        <td>${data.score}</td>
+    </tr>`;
+    leaderboard.innerHTML += row;
+  });
 }
-)
+
+displayLeaderboard()
